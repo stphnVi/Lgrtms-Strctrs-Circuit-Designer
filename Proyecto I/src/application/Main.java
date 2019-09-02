536acbf7b6c4e4920d574ffcb13bec144cf2c4f8
stphn1117 <47374699+stphn1117@users.noreply.github.com>
2019-09-01 20:16:32 -0500

    base

--- Proyecto I/src/application/Main.java
+++ Proyecto I/src/application/Main.java
@@ -4,28 +4,22 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
-/**/
+
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		//BorderPane root = new BorderPane();
-		
-		try {
+	public void start(Stage primaryStage) throws Exception{
+	
 			Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
 			Scene scene = new Scene(root,600,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
 }
+
--- kkk/src/application/Main.java
+++ kkk/src/application/Main.java
@@ -2,28 +2,20 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
-	
 	@Override
-	public void start(Stage primaryStage) {
-		BorderPane root = new BorderPane();
-		
-		try {
-			
-			Scene scene = new Scene(root,640,480);
-			scene.getStylesheets().add(getClass().getResource("/Base.fxml").toExternalForm());
+	public void start(Stage primaryStage) throws Exception{
+	
+			Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root,600,400);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-		
-		
 	}
 	
 	public static void main(String[] args) {
