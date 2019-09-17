5fc2a814a0661e3fd12892d0853328ca32405be0
stphn1117 <47374699+stphn1117@users.noreply.github.com>
2019-09-17 00:33:56 -0500

    inicio de las listas y la tabla

--- PWIntelliJ/src/application/Tabla.java
+++ PWIntelliJ/src/application/Tabla.java
@@ -1,5 +1,12 @@
 package application;
 
-public class Tabla {
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import java.awt.*;
 
+public class Tabla {
+    @FXML
+    TableView Tabla;
+        @FXML
+    Button Boperar;
 }
